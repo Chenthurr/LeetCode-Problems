e3154005d874cdd2cd6313f288d9041009023e1b
chenthurr <pchenthurr@gmail.com>
2026-01-31 20:36:50 +0530

    Time: 91 ms (0.46%), Space: 46.7 MB (9.86%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
@@ -1,22 +1,17 @@
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        int low=0, high=letters.length -1;
-        int mid;
-        int pos=-1;
-        while(low<=high)
-        {
-            mid = (low+high)/2;
-            if(letters[mid]>target)
-            {
-                pos=mid;
-                high = mid-1;
-            }
-            else
-            {
-                low=mid+1;
+        int left = 0;
+        int right = letters.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (letters[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-      return pos == -1 ? letters[0] : letters[pos];
 
+        return letters[left % letters.length];
     }
 }
